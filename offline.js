e4df43aff208c8189dcfb1338fdf3e38fd175626
ninevrza5597 <52026801+ninevrza5597@users.noreply.github.com>
2019-07-11 12:52:37 +0700

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,21 @@
+﻿{
+	"version": 1562824155,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/py1-sheet0.png",
+		"images/py1-sheet1.png",
+		"images/py1-sheet2.png",
+		"images/tiledbackground.png",
+		"images/bgg-sheet0.png",
+		"images/p-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
